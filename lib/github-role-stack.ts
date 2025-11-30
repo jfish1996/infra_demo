176c7567f8513259b5af081dbcd703bd8af36417
John Traut <john@Johns-MacBook-Air.local>
2025-11-30 12:01:21 -0800

    [Fix] role quick tests

--- lib/github-role-stack.ts
+++ lib/github-role-stack.ts
@@ -6,30 +6,25 @@ export class OidcRoleStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
-     
-    const providerArn = `arn:aws:iam::${this.account}:oidc-provider/token.actions.githubusercontent.com`;
-
-    // 👇 Import existing OIDC provider instead of creating one
-    const oidc = iam.OpenIdConnectProvider.fromOpenIdConnectProviderArn(
-      this,
-      "GitHubOIDC",
-      providerArn
-    );
+        const oidcProvider = new iam.OpenIdConnectProvider(this, "GitHubOIDC", {
+            url: "https://token.actions.githubusercontent.com",
+            clientIds: ["sts.amazonaws.com"],
+        });
 
         const githubActionsRole = new iam.Role(this, "GitHubActionsRole", {
-            roleName: "GitHub-ECS-Deploy", 
-          assumedBy: new iam.FederatedPrincipal(
-  oidc.openIdConnectProviderArn,
-  {
-    StringLike: {
-      "token.actions.githubusercontent.com:sub": "repo:jfish1996/infra_demo:*",
-    },
-    StringEquals: {
-      "token.actions.githubusercontent.com:aud": "sts.amazonaws.com"
-    }
-  },
-  "sts:AssumeRoleWithWebIdentity"
-),
+            roleName: "GitHub-ECS-Deploy",  // <- add this
+            assumedBy: new iam.FederatedPrincipal(
+                oidcProvider.openIdConnectProviderArn,
+                {
+                    StringLike: {
+                        "token.actions.githubusercontent.com:sub": "repo:jfish1996/infra_demo:*",
+                    },
+                    StringEquals: {
+                        "token.actions.githubusercontent.com:aud": "sts.amazonaws.com",
+                    },
+                },
+                "sts:AssumeRoleWithWebIdentity"
+            ),
             description: "Role assumed by GitHub Actions via OIDC",
         });
 
