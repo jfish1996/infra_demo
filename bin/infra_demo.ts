673623d45f78bbf286db3d86dc8ff5a59ae01d6a
John Traut <john@Johns-MacBook-Air.local>
2025-11-30 10:53:22 -0800

    [Fix] more adjustments to resolve first time role creation in account

--- bin/infra_demo.ts
+++ bin/infra_demo.ts
@@ -1,8 +1,16 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib/core';
 import { EcsServiceStack } from '../lib/ecs-stack';
-
+import { OidcRoleStack } from '../lib/github-role-stack';
 const app = new cdk.App();
+
+// First-time bootstrap stack for GitHub OIDC
+new OidcRoleStack(app, "OidcRoleStack", {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: "us-west-2",
+  }
+});
 
 
 new EcsServiceStack(app, 'EcsNodeService', {
@@ -11,3 +19,4 @@ new EcsServiceStack(app, 'EcsNodeService', {
     region: "us-west-2",
   }
 });
+
---
+++ lib/github-role-stack.ts
@@ -0,0 +1,31 @@
+import * as cdk from "aws-cdk-lib";
+import * as iam from "aws-cdk-lib/aws-iam";
+import { Construct } from "constructs";
+
+export class OidcRoleStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const oidcProvider = new iam.OpenIdConnectProvider(this, "GitHubOIDC", {
+      url: "https://token.actions.githubusercontent.com",
+      clientIds: ["sts.amazonaws.com"],
+    });
+
+    const githubActionsRole = new iam.Role(this, "GitHubActionsRole", {
+      assumedBy: new iam.FederatedPrincipal(
+        oidcProvider.openIdConnectProviderArn,
+        {
+          StringLike: {
+            "token.actions.githubusercontent.com:sub": "repo:jjtraut/infra_demo:*",
+          },
+        },
+        "sts:AssumeRoleWithWebIdentity"
+      ),
+      description: "Role assumed by GitHub Actions via OIDC",
+    });
+
+    githubActionsRole.addManagedPolicy(
+      iam.ManagedPolicy.fromAwsManagedPolicyName("AdministratorAccess")
+    );
+  }
+}
